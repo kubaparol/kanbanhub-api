0338e33d352d0b72bb407f621b3bd81a6b4a4aa3
Kuba Parol <99417488+kubaparol@users.noreply.github.com>
2024-07-11 22:11:03 +0200

    ft: add column module and related changes (#3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { DatabaseModule } from './database/database.module';
+import { ColumnModule } from './column/column.module';
 
 @Module({
-  imports: [BoardModule, DatabaseModule],
+  imports: [BoardModule, DatabaseModule, ColumnModule],
   controllers: [AppController],
   providers: [AppService],
 })
